13039d1f94fb82b62030902db69cc6aa2adbd7af
gurge <tarieligurgenidze@ibsu.edu.ge>
2025-11-26 16:46:51 +0400

    task 2.4 complete

---
+++ practice/src/app/models/item-filter.ts
@@ -0,0 +1,6 @@
+export interface ItemFilter {
+    model?: string | null;
+    priceFrom?: number | null;
+    priceTo?: number | null;
+    
+}
--- practice/src/app/shop-service.ts
+++ practice/src/app/shop-service.ts
@@ -1,6 +1,7 @@
 import { Injectable, model } from '@angular/core';
 import { ShopItem } from './models/shop-item';
 import { v4 } from 'uuid';
+import { ItemFilter } from './models/item-filter';
 
 @Injectable({
   providedIn: 'root',
@@ -25,9 +26,27 @@ export class ShopService {
     return this.items;
  }
 
- adItem(item: ShopItem) {
+ addItem(item: ShopItem) {
   item.id = v4();
   this.items.push(item);
   return item.id;
  }
+
+ filterItems(filter: ItemFilter) {
+  let result = this.items;
+
+  if(filter.model !== null && filter.model !== undefined && filter.model !== ''){
+    result = result.filter(c => c.modelName.includes(filter.model!));
+  }
+
+  if(filter.priceFrom!==null && filter.priceFrom !== undefined){
+    result = result.filter(c => c.price >= filter.priceFrom!);
+  }
+
+  if(filter.priceTo!==null && filter.priceTo !== undefined){
+    result = result.filter(c => c.price <= filter.priceTo!); 
+  }
+  
+  return result;
+ } 
 }
